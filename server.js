e3e0426bd4078b75c326742e35bbb68e7723ad26
RishiMadhavan-ui <rkmadhavan109@gmail.com>
2024-10-22 19:43:23 -0500

    removed next

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
 const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
 const { Server } = require("socket.io");
 
-const dev = process.env.NODE_ENV !== "production";
+// Set the hostname and port
 const hostname = "localhost";
 const port = 3001;
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+// Create an HTTP server
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Socket.IO server running\n");
+});
+
+// Allow CORS for Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Frontend URL
+    methods: ["GET", "POST"],
+  },
+});
+
+// Handle Socket.IO connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("room:join", ({ roomId, user }) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user:joined", user);
   });
 
-  // Allow CORS
-  const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000", // Frontend URL
-      methods: ["GET", "POST"],
-    },
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
+});
 
-  io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    socket.on("room:join", ({ roomId, user }) => {
-      socket.join(roomId);
-      socket.to(roomId).emit("user:joined", user);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-    });
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
+// Start the server
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://${hostname}:${port}`);
 });
